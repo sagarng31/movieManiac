25cc04cb656e67ef344006ccc9c37cc772b931e2
SagarMPatil <sagarng31@gmail.com>
2026-05-30 17:21:13 +0530

    added dark mode comp

---
+++ src/components/DarkMode/DarkMode.jsx
@@ -0,0 +1,17 @@
+import "./DarkMode.css";
+import Sun from "../../assets/Sun.svg?react";
+import Moon from "../../assets/Moon.svg?react";
+
+const DarkMode = () => {
+  return (
+    <div className="dark_mode">
+      <input className="dark_mode_input" type="checkbox" id="darkmode-toggle" />
+      <label className="dark_mode_label" htmlFor="darkmode-toggle">
+        <Sun />
+        <Moon />
+      </label>
+    </div>
+  );
+};
+
+export default DarkMode;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,12 +2,14 @@ import "./Navbar.css";
 import Fire from "../../assets/fire.png";
 import Star from "../../assets/glowing-star.png";
 import Party from "../../assets/partying-face.png";
+import DarkMode from "../DarkMode/DarkMode";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <h1>MovieManiac</h1>
       <div className="navbar_links">
+        <DarkMode />
         <a href="">
           Popular <img src={Fire} className="navbar_emoji" />
         </a>
